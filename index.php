f5eeac16faaec9783464072d0237bb6a520ba4d6
igar <yoho_ho@mail.ru>
2021-08-03 00:03:27 +0200

    module2-task1 Итерация по массивам

--- index.php
+++ index.php
@@ -55,9 +55,12 @@ $user_name = 'IgorGrinev'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
+			<?php $cats = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];?> 
+			<?php foreach ($cats as $value): ?> 
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $value; ?> </a>
             </li>
+			<?php endforeach; ?> 
         </ul>
     </section>
     <section class="lots">
@@ -66,17 +69,57 @@ $user_name = 'IgorGrinev'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
+			<?php $units = [
+					[
+					'name' => '2014 Rossignol District Snowboard',
+					'cat' => 'Доски и лыжи',
+					'price' => 10999,
+					'image' => 'img/lot-1.jpg'
+					],
+					[
+					'name' => 'DC Ply Mens 2016/2017 Snowboard',
+					'cat' => 'Доски и лыжи',
+					'price' => 159999,
+					'image' => 'img/lot-2.jpg'
+					],
+					[
+					'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+					'cat' => 'Крепления',
+					'price' => 8000,
+					'image' => 'img/lot-3.jpg'
+					],
+					[
+					'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+					'cat' => 'Ботинки',
+					'price' => 10999,
+					'image' => 'img/lot-4.jpg'
+					],
+					[
+					'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+					'cat' => 'Одежда',
+					'price' => 7500,
+					'image' => 'img/lot-5.jpg'
+					],
+					[
+					'name' => 'Маска Oakley Canopy',
+					'cat' => 'Разное',
+					'price' => 5400,
+					'image' => 'img/lot-6.jpg'
+					]
+				];
+			?>
+			<?php foreach ($units as $unit): ?> 
+			<li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?= $unit['image']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?= $unit['cat']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $unit['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $unit['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -84,6 +127,7 @@ $user_name = 'IgorGrinev'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+			<?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -93,9 +137,11 @@ $user_name = 'IgorGrinev'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+            <?php foreach ($cats as $value): ?> 
+			<li class="nav__item">
+                <a href="pages/all-lots.html"><?= $value; ?></a>
             </li>
+			<?php endforeach; ?> 
         </ul>
     </nav>
     <div class="main-footer__bottom container">
