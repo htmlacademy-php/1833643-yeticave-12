2a39402454b477362ecb4b7d41fc12e2e4755cb1
igar <yoho_ho@mail.ru>
2021-08-04 01:01:18 +0200

    Итерация по массивам.Исправление ошибок 2

--- index.php
+++ index.php
@@ -64,37 +64,37 @@ $user_name = 'IgorGrinev'; // укажите здесь ваше имя
                 $units = [
                     [
                         'name' => '2014 Rossignol District Snowboard',
-                        'cat' => 'Доски и лыжи',
+                        'category' => 'Доски и лыжи',
                         'price' => 10999,
                         'image' => 'img/lot-1.jpg'
                     ],
                     [
                         'name' => 'DC Ply Mens 2016/2017 Snowboard',
-                        'cat' => 'Доски и лыжи',
+                        'category' => 'Доски и лыжи',
                         'price' => 159999,
                         'image' => 'img/lot-2.jpg'
                     ],
                     [
                         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-                        'cat' => 'Крепления',
+                        'category' => 'Крепления',
                         'price' => 8000,
                         'image' => 'img/lot-3.jpg'
                     ],
                     [
                         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-                        'cat' => 'Ботинки',
+                        'category' => 'Ботинки',
                         'price' => 10999,
                         'image' => 'img/lot-4.jpg'
                     ],
                     [
                         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-                        'cat' => 'Одежда',
+                        'category' => 'Одежда',
                         'price' => 7500,
                         'image' => 'img/lot-5.jpg'
                     ],
                     [
                         'name' => 'Маска Oakley Canopy',
-                        'cat' => 'Разное',
+                        'category' => 'Разное',
                         'price' => 5400,
                         'image' => 'img/lot-6.jpg'
                     ]
@@ -123,7 +123,7 @@ $user_name = 'IgorGrinev'; // укажите здесь ваше имя
                             <img src="<?= $unit['image']; ?>" width="350" height="260" alt="">
                         </div>
                         <div class="lot__info">
-                            <span class="lot__category"><?= $unit['cat']; ?></span>
+                            <span class="lot__category"><?= $unit['category']; ?></span>
                             <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $unit['name']; ?></a>
                             </h3>
                             <div class="lot__state">
