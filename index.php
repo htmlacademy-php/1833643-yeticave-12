d170bb4e8db6ffa004aa80c50a70760490e06de7
igar <yoho_ho@mail.ru>
2021-08-28 23:17:59 +0200

    Создание схемы БД личного проекта

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require_once('helpers.php');
 define("TIMEZONE", "Europe/Kaliningrad");
-$is_auth = rand(0, 1);
-$user_name = 'IgorGrinev'; // укажите здесь ваше имя
+$isAuth = rand(0, 1);
+$userName = 'IgorGrinev'; // укажите здесь ваше имя
 
 //защита от XSS
 function e($string)
@@ -15,7 +15,7 @@ function e($string)
  * @param int $amount
  * @return string
  */
-function format_amount(int $amount): string
+function formatAmount(int $amount): string
 {
     $amount = ceil($amount);
     if ($amount >= 1000) {
@@ -29,15 +29,15 @@ function format_amount(int $amount): string
 
 /**
  *
- * @param string $fin_time => 'ГГГГ-ММ-ДД'
+ * @param string $finTime => 'ГГГГ-ММ-ДД'
  * @return array['$h' => 'string','$m' => 'string'])]
  *
  */
 #[ArrayShape (['$h' => 'string', '$m' => 'string'])]
-function countdown(string $fin_time): array //однообразил с ключом массива Units
+function countdown(string $finTime): array //однообразил с ключом массива Units
 {
     date_default_timezone_set(TIMEZONE);
-    $deadline = strtotime($fin_time);//дата истечения срока
+    $deadline = strtotime($finTime);//дата истечения срока
     $currentTime = time();//текущее время
     if ($deadline > $currentTime) {
         $s = $deadline - $currentTime;//осталось секунд до дедлайна
@@ -62,50 +62,50 @@ $units = [
         'name' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
-        'fin_time' => '2021-08-23',
+        'finTime' => '2021-08-23',
         'image' => 'img/lot-1.jpg'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
-        'fin_time' => '2021-08-18',
+        'finTime' => '2021-08-18',
         'image' => 'img/lot-2.jpg'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
-        'fin_time' => '2021-08-19',
+        'finTime' => '2021-08-19',
         'image' => 'img/lot-3.jpg'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
-        'fin_time' => '2021-08-20',
+        'finTime' => '2021-08-20',
         'image' => 'img/lot-4.jpg'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
-        'fin_time' => '2021-08-21',
+        'finTime' => '2021-08-21',
         'image' => 'img/lot-5.jpg'
     ],
     [
         'name' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
-        'fin_time' => '2021-08-22',
+        'finTime' => '2021-08-22',
         'image' => 'img/lot-6.jpg'
     ]
 ];
 $title = 'Главная';
-$page_content = include_template('main.php', compact('categories', 'units'));
+$pageContent = include_template('main.php', compact('categories', 'units'));
 $page = include_template(
     'layout.php',
-    compact('categories', 'categories', 'units', 'page_content', 'title', 'user_name', 'is_auth')
+    compact('categories', 'categories', 'units', 'pageContent', 'title', 'userName', 'isAuth')
 );
 print($page);
 ?>
--- schema.sql
+++ schema.sql
@@ -1,23 +1,25 @@
 CREATE
-DATABASE IF NOT EXISTS `yeticave` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DATABASE IF NOT EXISTS `yeticave` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE
-`yeticave`;
+  `yeticave`;
 
 CREATE TABLE `bets`
 (
-  `id`            int(11) NOT NULL,
+  `id`            int(11)   NOT NULL,
   `creation_time` timestamp NOT NULL,
-  `amount`        int(11) NOT NULL,
-  `id_users`      int(11) NOT NULL,
-  `id_lots`       int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `amount`        int(11)   NOT NULL,
+  `id_users`      int(11)   NOT NULL,
+  `id_lots`       int(11)   NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `categories`
 (
-  `id`          int(11) NOT NULL,
+  `id`          int(11)  NOT NULL,
   `name`        char(64) NOT NULL,
   `symbol_code` char(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 INSERT INTO `categories` (`id`, `name`, `symbol_code`)
 VALUES (1, 'Доски и лыжи', 'boards'),
@@ -29,30 +31,32 @@ VALUES (1, 'Доски и лыжи', 'boards'),
 
 CREATE TABLE `lots`
 (
-  `id`              int(11) NOT NULL,
+  `id`              int(11)      NOT NULL,
   `creation_time`   timestamp    NOT NULL,
   `name`            varchar(255) NOT NULL,
   `description`     text,
   `url_image`       varchar(255) NOT NULL,
   `initial_price`   double       NOT NULL,
   `completion_date` date         NOT NULL,
-  `bid_step`        int(11) NOT NULL,
-  `id_users_author` int(11) NOT NULL,
+  `bid_step`        int(11)      NOT NULL,
+  `id_users_author` int(11)      NOT NULL,
   `id_users_winner` int(11) DEFAULT NULL,
-  `id_categories`   int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_categories`   int(11)      NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `users`
 (
-  `id`                int(11) NOT NULL,
+  `id`                int(11)      NOT NULL,
   `registration_date` timestamp    NOT NULL,
   `email`             char(64)     NOT NULL,
   `name`              varchar(255) NOT NULL,
   `password`          varchar(255) NOT NULL,
   `contacts`          text,
-  `id_lots`           int(11) NOT NULL,
-  `id_bets`           int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_lots`           int(11)      NOT NULL,
+  `id_bets`           int(11)      NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 ALTER TABLE `bets`
   ADD PRIMARY KEY (`id`),
@@ -78,16 +82,17 @@ ALTER TABLE `users`
   ADD KEY `id_lots` (`id_lots`);
 
 ALTER TABLE `bets`
-  MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `categories`
-  MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 7;
 
 ALTER TABLE `lots`
-  MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `users`
-  MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `bets`
   ADD CONSTRAINT `bets_ibfk_1` FOREIGN KEY (`id_lots`) REFERENCES `lots` (`id`),
