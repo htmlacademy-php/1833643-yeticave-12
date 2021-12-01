ae4468779f1b4ce63350af976dd7dfa115efd0eb
igar <yoho_ho@mail.ru>
2021-12-02 00:29:43 +0200

    module6-task2

--- templates/main.php
+++ templates/main.php
@@ -16,16 +16,14 @@
         <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-        <!--заполните этот список из массива с товарами-->
-        <?php foreach ($units as $unit): ?>
+        <?php foreach($units as $unit): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="<?= $unit['image']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?= e($unit['category']); ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= e($unit['name']); ?></a>
-                    </h3>
+                    <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?= $unit['id'] ?>"><?= e($unit['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
